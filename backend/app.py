7e542573931842a1d1053f451da0030b97f9c40f
Adam Logan <adamlogan42@gmail.com>
2024-03-13 14:38:19 +0000

    Authenticates spotify API and gets user's playlists

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,68 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from os import environ
+import urllib.parse
+import base64
+import requests
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:5173")
 
 SPOTIFY_CLIENT_ID = environ.get('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = environ.get('SPOTIFY_CLIENT_SECRET')
+REDIRECT_SITE = environ.get('REDIRECT_SITE')
 
-@app.route('/api')
-def index():
+@app.route('/api', methods=['GET'])
+def test():
     print(SPOTIFY_CLIENT_ID)
     return jsonify({'name': 'alice',
-                       'email': 'alice@outlook.com'})
+                        'email': 'alice@outlook.com'})
+
+@app.route('/spotify/login-page', methods=['GET'])
+def spotifyLogin():
+    state = "HMFLKIUERNFKLJUR"
+    scope = 'user-read-currently-playing playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-recently-played user-library-modify user-read-private'
+    
+    params = urllib.parse.urlencode({
+        'response_type': 'code',
+        'client_id': SPOTIFY_CLIENT_ID,
+        'scope': scope,
+        'redirect_uri': REDIRECT_SITE,
+        'state': state
+    })
+    
+    return jsonify({'url' : f'https://accounts.spotify.com/authorize?{params}'})
+
+@app.route('/spotify/access-token/<code>', methods=['GET'])
+def getAccessToken(code):
+    print("code", code)
+    auth_header = base64.b64encode(f'{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}'.encode('utf-8')).decode('utf-8')
+
+    auth_options = {
+        'url': 'https://accounts.spotify.com/api/token',
+        'data': {
+            'code': code,
+            'redirect_uri': REDIRECT_SITE,
+            'grant_type': 'authorization_code'
+        },
+        'headers': {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': f'Basic {auth_header}'
+        }
+    }
+
+    return requests.post(**auth_options).json()
+
+# need to make sure all playlists are returned (there is a 'total' for number of playlists)
+# https://developer.spotify.com/documentation/web-api/reference/get-list-users-playlists
+@app.route('/spotify/all-playlists', methods=['GET'])
+def getAllPlaylists():
+    access_token = request.headers.get('Authorization').split()[1]
+
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    return requests.get('https://api.spotify.com/v1/me/playlists', headers=headers).json()
+
 app.run()
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,58 @@
-import { useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [response, setResponse] = useState('');
+	const apiUrl: string = import.meta.env.VITE_APP_API_URL;
+  const [playlists, setPlaylists] = useState([]);
 
-  const apiUrl:string = import.meta.env.VITE_APP_API_URL;
+  async function getAccessToken() {
+    const apiUrl: string = import.meta.env.VITE_APP_API_URL;
+  
+    try {
+      const spotifyAccess = await axios.get(
+        `${apiUrl}/spotify/access-token/${new URLSearchParams(window.location.search).get('code')}`
+      );
+  
+      console.log(spotifyAccess);
+  
+      localStorage.setItem('spotify_access_token', spotifyAccess.data.access_token);
+      console.log(spotifyAccess.data.access_token);
+      getPlaylists(spotifyAccess.data.access_token);
+  
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function getPlaylists(accessToken: string) {
+    console.log("foo", accessToken);
+    const apiUrl: string = import.meta.env.VITE_APP_API_URL;
+  
+    axios.get(`${apiUrl}/spotify/all-playlists`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then((response) => {
+      console.log(response);
+      setPlaylists(response.data.items);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    const code = new URLSearchParams(window.location.search).get('code');
+    if (code) {
+      getAccessToken();
+    }
+  }, []);
 
 	async function callApi() {
 		try {
-			const result = await axios.get(`${apiUrl}/api`);
-			setResponse(result.data);
+			const result = await axios.get(`${apiUrl}/spotify/login-page`);
+			window.location = result.data.url;
 		} catch (error) {
-			console.error('Error fetching data', error);
+			console.log(error);
 		}
 	}
 
@@ -19,7 +60,14 @@ function App() {
 		<>
 			<button onClick={() => callApi()}>Call API</button>
 
-			{response !== undefined && <p>{response.email}</p>}
+      <ul>
+        {
+          playlists.length === 0 && <li>Loading...</li>
+        }
+        {playlists.map((playlist:any) => (
+          <li key={playlist.id}>{playlist.name}</li>
+        ))}
+      </ul>    
 		</>
 	);
 }
